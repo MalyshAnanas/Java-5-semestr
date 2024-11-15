833af8846d50b33cc48aefe85d87bcc94bd98bf9
dudon <dudomkamalush@gmail.com>
2024-11-15 12:47:07 +0200

    add check_MATE and some fix

--- chess_java2024-main/src/Board.java
+++ chess_java2024-main/src/Board.java
@@ -26,12 +26,12 @@ public class Board {
                 new Pawn("P", 'b'),new Pawn("P", 'b'),new Pawn("P", 'b'),new Pawn("P", 'b')
         };
         this.fields[0] = new Figure[]{
-                new Rook("R", 'w'), new Knight("k", 'w'), new Bishop("B", 'w'), new King("K", 'w'),
-                new Queen("q", 'w'), new Bishop("B", 'w'), new Knight("k", 'w'), new Rook("R", 'w')
+                new Rook("R", 'w'), new Knight("k", 'w'), new Bishop("B", 'w'), new Queen("Q", 'w'),
+                new King("K", 'w'), new Bishop("B", 'w'), new Knight("k", 'w'), new Rook("R", 'w')
         };
         this.fields[7]= new Figure[]{
-                new Rook("R", 'b'), new Knight("k", 'b'), new Bishop("B", 'b'), new King("K", 'b'),
-                new Queen("q", 'b'), new Bishop("B", 'b'), new Knight("k", 'b'), new Rook("R", 'b')
+                new Rook("R", 'b'), new Knight("k", 'b'), new Bishop("B", 'b'), new Queen("Q", 'b'),
+                new King("K", 'b'), new Bishop("B", 'b'), new Knight("k", 'b'), new Rook("R", 'b')
         };
     }
 
@@ -79,9 +79,10 @@ public class Board {
         Figure figure = this.fields[row][col];
         if(figure != null && figure.canMove(row, col, row1, col1) && this.fields[row1][col1] == null && figure.getColor() == this.colorGame){
             if (figure.getName()=="B" || figure.getName()=="Q"){
-                for (int i=Math.min(col, col1); i<Math.max(col, col1); i++){
-                    for(int j=Math.min(row, row1); j<Math.max(row, row1); j++){
-                        if(Math.abs(i-j)==0 && this.fields[i][j] != null){
+                for (int i=Math.min(col, col1)+1; i<Math.max(col, col1); i++){
+                    for(int j=Math.min(row, row1)+1; j<Math.max(row, row1); j++){
+                        if(Math.abs(i-col)==Math.abs(j-row) && this.fields[j][i] != null){
+                            System.out.print("Мешает фигура!!!!!");
                             return false;
                         }
                     }
@@ -89,15 +90,17 @@ public class Board {
             }
             if (figure.getName()=="R" || figure.getName()=="Q"){
                 if (col==col1){
-                    for(int j=Math.min(row, row1); j<Math.max(row, row1); j++){
+                    for(int j=Math.min(row, row1)+1; j<Math.max(row, row1); j++){
                         if(this.fields[j][col] != null){
+                            System.out.print("Мешает фигура!!!!!");
                             return false;
                         }
                     }
                 }
                 if (row==row1){
-                    for(int i=Math.min(col, col1); i<Math.max(col, col1); i++){
+                    for(int i=Math.min(col, col1)+1; i<Math.max(col, col1); i++){
                         if(this.fields[row][i] != null){
+                            System.out.print("Мешает фигура!!!!!");
                             return false;
                         }
                     }
@@ -107,7 +110,7 @@ public class Board {
         return true;
     }
 
-    private boolean check_SHAH (int row, int col, int row1, int col1){
+    private boolean check_SHAH (int row, int col, int row1, int col1){ //если шаха нет, то возвращает true
         Figure figure = this.fields[row][col];
         if (figure != null && figure.canMove(row, col, row1, col1) && this.figure_on_way(row, col, row1, col1)
                 && this.fields[row1][col1] == null && figure.getColor() == this.colorGame){
@@ -117,6 +120,7 @@ public class Board {
                         for (int w=0; w<8; w++){ // ищем фигуры, которые могут поставить шах
                             for (int e=0; e<8; e++){
                                 if(this.fields[w][e].canAttack(w, e, i, q) && this.fields[w][e].getColor()!=this.colorGame){
+                                    System.out.print("======ШАХ======");
                                     this.fields[row1][col1] = figure;
                                     this.fields[row][col] = null;
                                     for (int z=0; z<8; z++){
@@ -144,4 +148,34 @@ public class Board {
         return false;
     }
 
+    private boolean check_MATE (int row, int col, int row1, int col1){ //если мата нет, то возвращает true
+        Figure figure = this.fields[row][col];
+        if (!this.check_SHAH(row, col, row1, col1) && figure != null && this.fields[row1][col1] == null){
+            for (int i=0; i<8; i++){
+                for (int j=0; j<8; j++){
+                    if (this.fields[i][j] != null && this.fields[i][j].getName()=="K" && this.fields[i][j].getColor()==this.colorGame){ //ищем короля
+                        for (int q = Math.max(i - 1, 0); q <= Math.min(i + 1, 7); q++) {
+                            for (int w = Math.max(j - 1, 0); w <= Math.min(j + 1, 7); w++) {
+                                if (this.check_SHAH(i, j, q, w)){
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                    if (this.fields[i][j] != null && this.fields[i][j].getName()!="K" && this.fields[i][j].getColor()==this.colorGame){ //ищем фигуру "защитника"
+                        for (int q=0; q<8; q++){
+                            for (int w=0; w<8; w++){ //ищем ход для защиты короля
+                                if (this.check_SHAH(i,j,q,w)){
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        System.out.print("_____ШАХ и МАТ_____");
+        return false;
+    }
 }
--- chess_java2024-main/src/Main.java
+++ chess_java2024-main/src/Main.java
@@ -55,7 +55,5 @@ public class Main {
                 case 'b': board.setColorGame('w');break;
             }
         }
-        //test
-
     }
 }
